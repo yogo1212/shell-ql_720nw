e6f0b1091e8e98a71365129a3c412fbe8d226c7b
Leon M. George <leon@georgemail.eu>
2022-10-13 09:21:16 +0200

    bool_outputter: spaces -> tabs

--- bool_outputter.rb
+++ bool_outputter.rb
@@ -1,19 +1,19 @@
 require 'barby/outputter'
 
 module Barby
-  #Outputs an bool representation of the barcode.
-  #True bool is a black pixel. Works only for 1D-barcodes.
-  #
-  #Registers to_bool
-  class BoolOutputter < Outputter
-    register :to_bool
+	#Outputs an bool representation of the barcode.
+	#True bool is a black pixel. Works only for 1D-barcodes.
+	#
+	#Registers to_bool
+	class BoolOutputter < Outputter
+		register :to_bool
 
-    def to_bool()
-      if barcode.two_dimensional?
+		def to_bool()
+			if barcode.two_dimensional?
 				nil
-      else
-        booleans
-      end
-    end
-  end
+			else
+				booleans
+			end
+		end
+	end
 end
